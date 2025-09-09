0b005cf4b3fbb059f09aa8f7f9994734b7bd34d6
John Donovan <john@geospark.co.uk>
2025-09-09 21:25:04 +0100

    Seeks nearest food

--- snake_move.py
+++ snake_move.py
@@ -1,21 +1,28 @@
 import json
 import typing
 
-from neighbours import game_state_example, get_graph
+from neighbours import game_state_example, get_graph, get_neighbours
 from snake_astar import SnakeAStar
 
 
 def move_astar(game_state: typing.Dict, graph: list) -> typing.Dict:
     # Aim for the first food in the list.
-    goal = game_state['board']['food']
+    foods = game_state['board']['food']
+    head = (game_state["you"]["head"]["x"], game_state["you"]["head"]["y"])
 
     # Or aim somewhere else. Head for the centre of the board :shrug:
-    if len(goal) == 0:
-        goal = (5, 5)
-    else:
-        goal = (goal[0]['x'], goal[0]['y'])
+    goal = (5, 5)
+    dist = 99999
 
-    head = (game_state["you"]["head"]["x"], game_state["you"]["head"]["y"])
+    for food in foods:
+        f1 = (food['x'], food['y'])
+        d1 = abs(f1[0] - head[0]) + abs(f1[1] - head[1])
+
+        if d1 < dist:
+            dist = d1
+            goal = f1
+
+    get_neighbours(head, game_state)
 
     path = SnakeAStar(graph).astar(head, goal)
 
